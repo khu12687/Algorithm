4fbd78b25d41d1c924b043c2d79faa35cb941f84
khu12687 <khu12687@naver.com>
2022-09-13 23:27:24 +0900

    단어정렬

---
+++ class2/RangeWord.java
@@ -0,0 +1,34 @@
+package class2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class RangeWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String[] arr = new String[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.next();
+        }
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (s1.length() == s2.length()) {
+                    return s1.compareTo(s2);
+                } else {
+                    return s1.length() - s2.length();
+                }
+            }
+        });
+        System.out.println(arr[0]);
+        for (int i = 1; i < N; i++) {
+            if (!arr[i].equals(arr[i - 1])) {
+                System.out.println(arr[i]);
+            } else {
+                continue;
+            }
+        }
+    }
+}
